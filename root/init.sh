672ff06b85b4bb1c355574a6ef2cea675ab60e94
xiaozhuai <798047000@qq.com>
2021-05-03 00:33:55 +0800

    fix permissions

--- root/init.sh
+++ root/init.sh
@@ -4,32 +4,16 @@ set -e
 UID=`id -u`
 GID=`id -g`
 
-echo
 echo "UID: $UID"
-echo "GID: $GID"
-echo
+echo "UID: $GID"
 
-
-echo "Setting owner and permissions"
-
-chown -R $UID:$GID /config
-find /config -type d -exec chmod 755 {} +
-find /config -type f -exec chmod 644 {} +
-
-echo "Setting conf"
+chmod 755 /config
+chmod 755 /downloads
 
 touch /config/aria2.session
-
 if [[ ! -e /config/aria2.conf ]]
 then
   cp /aria2.conf.default /config/aria2.conf
 fi
 
-echo "Starting aria2c"
-
-exec aria2c \
-    --conf-path=/config/aria2.conf \
-  > /dev/stdout \
-  2 > /dev/stderr
-
-echo 'Exiting aria2'
+aria2c --conf-path=/config/aria2.conf > /dev/stdout 2 > /dev/stderr
